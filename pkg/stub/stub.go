d189194377f7817cb52e23e68037248dd98c023b
lqw@wsl <lqw332664203@mail.ustc.edu.cn>
2026-02-17 00:22:39 +0800

    feat: use *bool for markStub and support partial marking by markStub *bool field

--- pkg/stub/stub.go
+++ pkg/stub/stub.go
@@ -1,10 +1,8 @@
 package stub
 
-import "unsafe"
-
 var markStub bool
 
 // GetMarkStubAddr return address of markStub. Objects with this value will be handled specially
-func GetMarkStubAddr() uintptr {
-	return uintptr(unsafe.Pointer(&markStub))
+func GetMarkStubAddr() *bool {
+	return &markStub
 }
