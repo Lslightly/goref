bb0106af3140091cdd515c159e59be65fe7a55b1
lqw@wsl <lqw332664203@mail.ustc.edu.cn>
2026-07-25 22:14:44 +0800

    refactor: recover order of flushReference and location, function writing

--- pkg/proc/protobuf.go
+++ pkg/proc/protobuf.go
@@ -322,6 +322,7 @@ func (b *profileBuilder) pbMapping(tag int, id, base, limit, offset uint64, file
 }
 
 func (b *profileBuilder) flush() {
+	b.flushReference()
 	for i := uint64(5); i < uint64(len(b.strings)); i++ {
 		// write location
 		start := b.pb.startMessage()
@@ -335,7 +336,6 @@ func (b *profileBuilder) flush() {
 		b.pb.int64Opt(tagFunction_Name, int64(i))
 		b.pb.endMessage(tagProfile_Function, start)
 	}
-	b.flushReference()
 	// just avoid error msg from pprof tool
 	b.pbMapping(tagProfile_Mapping, 1, uint64(0), uint64(0xff), 0, "", "", false)
 	b.pb.strings(tagProfile_StringTable, b.strings)
